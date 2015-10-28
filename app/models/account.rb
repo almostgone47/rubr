559af43bdee8976a2f3ec2ac64dc3b1776013c01
brandonsturgeon <sturgeonb4@gmail.com>
2015-10-28 15:09:55 -0500

    Increases max user_name length

--- app/models/account.rb
+++ app/models/account.rb
@@ -10,7 +10,7 @@ class Account < ActiveRecord::Base
   before_create { generate_token(:auth_token) }
 
   validates_presence_of :email, :first_name, :last_name, :zip, :looking_for, :gender, :age
-  validates :user_name, uniqueness: { case_sensitive: false }, length: { maximum: 16 }
+  validates :user_name, uniqueness: { case_sensitive: false }, length: { maximum: 24 }
   validates_numericality_of :age, only_integer: true,
                                   greater_than: 17,
                                   less_than: 100
