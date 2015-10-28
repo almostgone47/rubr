d72cf00130954b464084b9e067650cda088da93f
brandonsturgeon <sturgeonb4@gmail.com>
2015-10-28 15:05:46 -0500

    Refactors creating fake accounts

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require "#{File.dirname(__FILE__)}/../lib/fake_account_service.rb"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
