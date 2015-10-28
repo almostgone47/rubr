ef9cbc1ee3f7ee1f077a0946926c257311b5dc6b
brandonsturgeon <sturgeonb4@gmail.com>
2015-10-28 14:43:06 -0500

    Adds error handling for getting people

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,7 +26,7 @@ class PagesController < ApplicationController
     rescue => error
       puts error.inspect
       puts "Rescued"
-      status = 727
+      status = 500
     end
 
     respond_to do |format|
@@ -55,7 +55,7 @@ class PagesController < ApplicationController
     rescue => error
       puts error.inspect
       puts "Rescued"
-      status = 727
+      status = 500
     end
 
     respond_to do |format|
@@ -66,25 +66,48 @@ class PagesController < ApplicationController
 
   # Get all unrated people (API call from dashboard)
   def get_people
-    cur_account = current_account
-    puts "-----------"
-    puts "Request for people by user #{cur_account.id}"
+    begin
+      cur_account = current_account
+      puts "-----------"
+      puts "Request for people by user #{cur_account.id}"
 
-    rated_ids = cur_account.ratings.pluck(:id)
-    rated_ids.push(cur_account.id)
+      rated_ids = cur_account.ratings.pluck(:id)
+      rated_ids.push(cur_account.id)
 
-    looking_for = cur_account.looking_for
-    if looking_for == "any"
-      looking_for = ["male", "female"]
+      looking_for = cur_account.looking_for
+      if looking_for == "any"
+        looking_for = ["male", "female"]
+      end
+
+      people = Account.where(gender: cur_account.looking_for).where.not(id: rated_ids).to_a
+
+      # Only pass certain params that the page will use
+      # TODO: write this better
+      people_data = []
+      people.each do |person|
+        person_struct = {
+            id: person.id,
+            profile_image: person.profile_image,
+            age: person.age,
+            description: person.description,
+            username: person.user_name
+        }
+        people_data.push(person_struct)
+      end
+
+      puts "Retrieved #{people.count} people"
+      puts "-----------"
+
+      respond_to do |format|
+        format.json { render json: people }
+      end
+
+    rescue => e
+      puts "Error getting people"
+      puts e.inspect
+      render :status => 500
     end
 
-    people = Account.where(gender: cur_account.looking_for).where.not(id: rated_ids).to_a
 
-    puts "Retrieved #{people.count} people"
-    puts "-----------"
-
-    respond_to do |format|
-      format.json { render json: people}
-    end
   end
 end
