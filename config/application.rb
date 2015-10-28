49deb1b8f62690baf70708d0a71864e0cb73da6d
brandonsturgeon <sturgeonb4@gmail.com>
2015-10-28 13:56:30 -0500

    Will this do anything?

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Rubr
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 end
